002c08e3cc5bc9ece576d5f2d7f8ca2f363799e1
Rahul2783 <sahurahul2783@gmail.com>
2025-01-09 16:15:14 +0530

    EVParameter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import EVParameters from './Components/EVParameters'
+
+
+function App() {
+
+  return (
+    <>
+    <h2 className='font-bold text-3xl flex justify-center p-5'>I have use tailwind CSS</h2>
+      <EVParameters/>
+    </>
+  )
+}
+
+export default App
